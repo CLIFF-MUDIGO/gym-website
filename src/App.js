c4f8e7c933f212cc3d98158e1d379553ad232c52
cliff <cliffmudigo5@gmail.com>
2023-10-16 23:56:59 +0300

    :sparkles: feat: added features functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Feature from './components/Feature';
 
 const App = () => {
   return (
     <div className="App">
        <Navbar />
        <Header />
+       <Feature />
     </div>
   );
 }
