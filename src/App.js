18416f182e1bde9370085b2636e5d5e73ac9c380
cliff <cliffmudigo5@gmail.com>
2023-10-17 00:11:31 +0300

    :sparkles: feat: added offer file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Feature from './components/Feature';
+import Offer from './components/Offer';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
        <Navbar />
        <Header />
        <Feature />
+       <Offer />
     </div>
   );
 }
